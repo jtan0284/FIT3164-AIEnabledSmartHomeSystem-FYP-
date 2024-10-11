3a155d42a3f45d165b1f7f1151f0ef72df3dd584
Jian Tan <jtan0284@student.monash.edu>
2024-10-11 07:20:36 +0000

    Add new file

---
+++ OpenVLC/TX Script/run_OpenVLC.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Change to the directory where the driver is located
+cd /home/debian/OpenVLC/Latest_Version/Driver/
+
+# Run the load.sh script with sudo privileges
+sudo ./load_test.sh
+
+# Navigate to the PRU TX directory
+cd ..
+cd /home/debian/OpenVLC/Latest_Version/PRU/TX
+
+# Run the deploy.sh script with sudo privileges
+sudo ./deploy.sh
+
+# Return to the home directory
+cd ~
